21ad6f97f8fb5d3db8dfcafca20450f9a4d14305
SowvikSuvo <skarmoker44@gmail.com>
2025-10-21 13:02:37 +0600

    Navigate component added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { format } from "date-fns";
 
 const Header = () => {
   return (
-    <div className="flex justify-center flex-col items-center gap-3">
+    <div className="flex justify-center flex-col items-center gap-3 mt-5">
       <img className="w-[350px]" src={logo} alt="" />
       <p className="text-accent">Journalism Without Fear or Favour</p>
       <p className="font-semibold text-accent">
